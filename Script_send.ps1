5e65e3864ce55b725374aed8a617f9ac0d84a705
Shehu Ibrahim <33332955+shehu-babs@users.noreply.github.com>
2024-11-25 15:25:21 -0500

    Update Script_send.ps1

--- Script_send.ps1
+++ Script_send.ps1
@@ -1,4 +1,4 @@
-﻿# Define the body of the message with required fields
+﻿# Define the body of the message with the required fields
 $body = @{
     "@type" = "MessageCard"
     "@context" = "http://schema.org/extensions"
@@ -51,7 +51,7 @@ $body = @{
 # Convert the body to a JSON string with proper depth
 $json = $body | ConvertTo-Json -Depth 4
 
-# Define the URI for the webhook(Make sure you have a live url)
+# Define the URI for the webhook(Make sure you have a live URL)
 $uri = 'https://centralmichigan.webhook.office.com/webhookb2/7a5f7f32-9868-41d9-8e78-e1dbe653c091@c871bc6e-7cc6-4a57-a4eb-22309fc34963/IncomingWebhook/d4ded51e79794265a1275e76016d30db/23e8147e-c132-4752-b144-8e60829afe4c/V2ZxkOYV-gi9fpIVwZf9vWls_A5gO37skhGcjyKVSx_iI1'
 
 # Use Invoke-RestMethod for sending the JSON payload to the webhook
